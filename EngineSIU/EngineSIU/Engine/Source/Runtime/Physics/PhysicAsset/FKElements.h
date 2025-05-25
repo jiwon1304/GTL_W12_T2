5de1028c4a69faf1688abf4538bad84b61783d4d
chanhuiHan <hch3245@naver.com>
2025-05-25 15:09:25 +0900

    WIP: FKElements 생성자들 추가 작업

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Physics/PhysicAsset/FKElements.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Physics/PhysicAsset/FKElements.h
@@ -29,12 +29,50 @@ struct FKShapeElem
     ECollisionEnabled::Type CollisionEnabled;
 
     // FUserData UserData;
+
+    FKShapeElem()
+        : ShapeType(EAggCollisionShape::Unknown)
+        , bContributeToMass(true)
+        , CollisionEnabled(ECollisionEnabled::QueryAndPhysics)
+    {
+    }
+
+    FKShapeElem(EAggCollisionShape::Type InShapeType)
+        : ShapeType(InShapeType)
+        , bContributeToMass(true)
+        , CollisionEnabled(ECollisionEnabled::QueryAndPhysics)
+    {
+    }
 };
 
 struct FKSphereElem : public FKShapeElem
 {
     FVector Center;
     float Radius;
+
+    FKSphereElem()
+        : FKShapeElem(EAggCollisionShape::Sphere)
+        , Center(FVector::ZeroVector)
+        , Radius(1)
+    {
+
+    }
+
+    FKSphereElem(float r)
+        : FKShapeElem(EAggCollisionShape::Sphere)
+        , Center(FVector::ZeroVector)
+        , Radius(r) 
+    {
+
+    }
+
+    FKSphereElem(float r, FVector InCenter)
+        : FKShapeElem(EAggCollisionShape::Sphere)
+        , Center(InCenter)
+        , Radius(r) 
+    {
+
+    }
 };
 
 struct FKBoxElem : public FKShapeElem
@@ -44,6 +82,44 @@ struct FKBoxElem : public FKShapeElem
     float X;    // Extent of the box
     float Y;
     float Z;
+
+    FKBoxElem()
+        :FKShapeElem(EAggCollisionShape::Box)
+        , Center(FVector::ZeroVector)
+        , Rotation(FRotator::ZeroRotator)
+        , X(1), Y(1), Z(1)
+    {
+
+    }
+
+    FKBoxElem(float s)
+        :FKShapeElem(EAggCollisionShape::Box)
+        , Center(FVector::ZeroVector)
+        , Rotation(FRotator::ZeroRotator)
+        , X(s), Y(s), Z(s)
+    {
+
+    }
+
+    FKBoxElem(float InX, float InY, float InZ)
+        :FKShapeElem(EAggCollisionShape::Box)
+        , Center(FVector::ZeroVector)
+        , Rotation(FRotator::ZeroRotator)
+        , X(InX), Y(InY), Z(InZ)
+    {
+
+    }
+
+    FTransform GetTransform() const
+    {
+        return FTransform(Rotation.Quaternion(), Center);
+    }
+
+    void SetTransform(const FTransform& InTransform)
+    {
+        Rotation = InTransform.Rotator();
+        Center = InTransform.GetTranslation();
+    }
 };
 
 struct FKSphylElem : public FKShapeElem 
@@ -52,6 +128,35 @@ struct FKSphylElem : public FKShapeElem
     FRotator Rotation;
     float Radius;
     float Length;
+
+    FKSphylElem()
+        : FKShapeElem(EAggCollisionShape::Sphyl)
+        , Center(FVector::ZeroVector)
+        , Rotation(FRotator::ZeroRotator)
+        , Radius(1), Length(1)
+    {
+
+    }
+
+    FKSphylElem(float InRadius, float InLength)
+        : FKShapeElem(EAggCollisionShape::Sphyl)
+        , Center(FVector::ZeroVector)
+        , Rotation(FRotator::ZeroRotator)
+        , Radius(InRadius), Length(InLength)
+    {
+
+    }
+
+    FTransform GetTransform() const 
+    {
+        return FTransform(Rotation.Quaternion(), Center);
+    }
+
+    void SetTransform(const FTransform& InTransform) 
+    {
+        Rotation = InTransform.Rotator();
+        Center = InTransform.GetTranslation();
+    }
 };
 
 struct FKConvexElem : public FKShapeElem 
@@ -60,4 +165,14 @@ struct FKConvexElem : public FKShapeElem
     TArray<int32> IndexData;
     FBoundingBox ElemBox;
     FTransform Transform;
+
+    FTransform GetTransform() const 
+    {
+        return Transform;
+    }
+
+    void SetTransform(const FTransform& InTransform) 
+    {
+        Transform = InTransform;
+    }
 };
